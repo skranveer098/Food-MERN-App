d20553708243d62ff3b7888a83183280a79acdc0
Ranveer Singh <101648494+skranveer098@users.noreply.github.com>
2024-08-01 01:40:48 +0530

    Update Login.js
    changed link in login

--- frontend/src/screens/Login.js
+++ frontend/src/screens/Login.js
@@ -7,7 +7,7 @@ export default function Login() {
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-    const response = await fetch("http://localhost:5000/api/auth/login", {
+    const response = await fetch("https://food-mern-app.vercel.app/", {
       // credentials: 'include',
       // Origin:"http://localhost:3000/login",
       method: 'POST',
