3f15bf6e607773d99fb760f91c8361d25b549679
Ranveer Singh <101648494+skranveer098@users.noreply.github.com>
2024-08-01 01:55:42 +0530

    Update Cart.js
    end time link change

--- frontend/src/screens/Cart.js
+++ frontend/src/screens/Cart.js
@@ -19,7 +19,7 @@ export default function Cart() {
   const handleCheckOut = async () => {
     let userEmail = localStorage.getItem("userEmail");
     // console.log(data,localStorage.getItem("userEmail"),new Date())
-    let response = await fetch("https://food-mern-app.vercel.app/", {
+    let response = await fetch("https://food-mern-app.vercel.app/api/auth/orderData", {
       // credentials: 'include',
       // Origin:"http://localhost:3000/login",
       method: 'POST',
